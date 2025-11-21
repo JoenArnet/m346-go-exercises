eabedade545f75bf3528b643640b61c7147a6f50
Joen Arnet <joen_arnet@sluz.ch>
2025-11-21 15:03:30 +0100

    go 1, ex 1-3

--- go-1-ex-1/main.go
+++ go-1-ex-1/main.go
@@ -4,6 +4,13 @@ import "fmt"
 
 func main() {
 	// TODO: Declare and initialize the variables being used in the output!
+	var firstName string = "Joen"
+	var lastName string = "Arnet"
+	var dayOfBirth, monthOfBirth, yearOfBirth = 10, 9, 2008
+	var numberOfSiblings int = 1
+	var heightInMeters float32 = 1.73
+	var zodiacSign rune = '\u264D'
+
 	fmt.Printf("Vor- und Nachname: %s %s\n", firstName, lastName)
 	fmt.Printf("Geburtsdatum: %d.%d.%d\n", dayOfBirth, monthOfBirth, yearOfBirth)
 	fmt.Printf("Anzahl Geschwister: %d\n", numberOfSiblings)
