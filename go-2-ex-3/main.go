32fd317e62470c7b27aa6c0163e52c38bf9a3ccf
Joen Arnet <joen_arnet@sluz.ch>
2025-11-28 15:06:58 +0100

    go-2-ex-3

--- go-2-ex-3/main.go
+++ go-2-ex-3/main.go
@@ -4,13 +4,18 @@ import "fmt"
 
 func main() {
 	// TODO: create a map called "modules"
+	modules := make(map[int]string)
 
 	fmt.Println("Modul 104:", modules[104])
 	fmt.Println("Modul 117:", modules[117])
 	fmt.Println("Modul 346:", modules[346])
 
 	// TODO: delete one
+	delete(modules, 104)
 	// TODO: add one
+	modules[320] = "Modul 320:"
 	// TODO: replace one
+	modules[117] = "Neues Modul 117:"
+
 	fmt.Println(modules)
 }
